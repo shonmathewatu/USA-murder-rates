275678a1e414db3e79cebd222ba3469d8e7b55a7
Shon Mathew - STUDENT <l00171122@atu.ie>
2023-05-16 14:48:28 +0100

    initial

--- script.R
+++ script.R
@@ -1,40 +1,154 @@
-install.packages("state.x77")
+# Practical 9
 
-#preperocessing
-str(state.x77)
-class(state.x77)
-
-#converting matrix array to df
-
+# Q1
 states <- as.data.frame(state.x77)
-
-
 str(states)
 
-#murders will be the dependet variable
-help("state.x77")
+# Renaming Life Exp and HS Grad variables as 
+# these will cause possible issues when referring to
+# them since they contain a space.
+colnames(states)[colnames(states) == "Life_Exp"] <- "Life_Exp"
+colnames(states)[colnames(states) == "HS_Grad"] <- "HS_Grad"
 
-pairs(states)
+# Q3a
+# Examine initial linearity between variables in the dataset
+library(psych)
+pairs.panels(states,
+             smooth = FALSE,      # If TRUE, draws loess smooths
+             scale = FALSE,      # If TRUE, scales the correlation text font
+             density = TRUE,     # If TRUE, adds density plots and histograms
+             ellipses = FALSE,    # If TRUE, draws ellipses
+             method = "spearman",# Correlation method (also "pearson" or "kendall")
+             pch = 21,           # pch symbol
+             lm = FALSE,         # If TRUE, plots linear fit rather than the LOESS (smoothed) fit
+             cor = TRUE,         # If TRUE, reports correlations
+             jiggle = FALSE,     # If TRUE, data points are jittered
+             factor = 2,         # Jittering factor
+             hist.col = 4,       # Histograms color
+             stars = TRUE,       # If TRUE, adds significance level with stars
+             ci = TRUE)          # If TRUE, adds confidence intervals
 
+# Examine linearity in more detail
+scatter.smooth(x = states$Population,
+               y = states$Murder,
+               xlab = "Population (,000)",
+               ylab = "Murder %", main = "Correlation of murder ~ population")
 
+# chart appears to have medium correlation
+# low correlation (-0.2 < x < 0.2) suggests that 
+# much of variation of the response
+# variable (Y) is unexplained by the predictor (X)
+# in which case, we should probably look for better
+# explanatory variables
+cor(states$Murder, states$Population)
+
+scatter.smooth(x = states$Illiteracy,
+               y = states$Murder,
+               main = "Correlation of Murder ~ Illiteracy",
+               xlab = "Illiteracy %",
+               ylab = "Murder %")
+
+# Examining correlation between murder and illiteracy
+cor(states$Murder, states$Illiteracy)
+
+# This is a better correlation value between both variables.
+# Lets examine murder and frost variables for correlation.
+scatter.smooth(x = states$Frost,
+               y = states$Murder,
+               main = "Correlation of Murder ~ Frost",
+               xlab = "Frost",
+               ylab = "Murder %")
+cor(states$Murder, states$Frost)
+
+# Examining the other variables
+paste("Correlation for Murder and Frost: ", cor(states$Murder, states$Frost))
+paste("Correlation for Murder and Illiteracy: ", cor(states$Murder, states$Illiteracy))
+paste("Correlation for Murder and Population: ", cor(states$Murder, states$Population))
+paste("Correlation for Murder and HS Grad: ", cor(states$Murder, states$HS_Grad))
+paste("Correlation for Murder and Income: ", cor(states$Murder, states$Income))
+paste("Correlation for Murder and Life Exp: ", cor(states$Murder, states$Life_Exp))
+paste("Correlation for Murder and Area: ", cor(states$Murder, states$Area))
+
+# It appears that the variable Area has a vary low correlation with Murder. 
+# Therefore I am going to remove it from the dataset. 
+# Alternatively we can choose to exclude these independent variables when
+# we are constructing the MLR model.
+
+# Q3b
+# Check for outliers
+opar <- par(no.readonly = TRUE)
+par(mfrow = c(4, 2)) # divide graph area in 3 rows by 2 columns
 attach(states)
-
-
-scatter.smooth(x = Population,
-               y = Murder,
-               main = "Murder vs Population",
-               xlab = "Population",
-               ylab = "Murder")
-
-cor(Murder,Population)
-
-# -0.2< x < 0.2 then most of ther esponse os the var
-# s not predicted by the predictor variable
-
+boxplot(Murder,
+        main = "Murder",
+        sub = paste("Outlier rows: ",
+                    boxplot.stats(Murder)$out)) # box plot for 'murder'
 boxplot(Population,
-        main = "poplulation",
-        sub = paste("outlier rows:", boxplot.stats(Population))
+        main = "Population",
+        sub = paste("Outlier rows: ",
+                    boxplot.stats(Population)$out)) # box plot for 'Population'
+boxplot(states$HS_Grad,
+        main = "Graduation",
+        sub = paste("Outlier rows: ",
+                    boxplot.stats(states$HS_Grad)$out)) # box plot for 'HS Grad'
+boxplot(Illiteracy,
+        main = "Illiteracy",
+        sub = paste("Outlier rows: ",
+                    boxplot.stats(Illiteracy)$out)) # box plot for 'HS Grad'
+boxplot(Income,
+        main = "Income",
+        sub = paste("Outlier rows: ",
+                    boxplot.stats(Income)$out)) # box plot for 'HS Grad'
+boxplot(Frost,
+        main = "Frost",
+        sub = paste("Outlier rows: ",
+                    boxplot.stats(Frost)$out)) # box plot for 'HS Grad'
+boxplot(states$Life_Exp,
+        main = "Life Exp",
+        sub = paste("Outlier rows: ",
+                    boxplot.stats(states$Life_Exp)$out)) # box plot for 'HS␣Grad'
+detach(states)
+par(opar)
 
-        
-        
-        
+# Both the population and Income variables contain outliers.
+# Use boxplot.stats() function to generate relevant outliers
+outlier_values <- boxplot.stats(states$Population)$out # outlier values.
+paste("Population outliers: ", paste(outlier_values, collapse=", "))
+
+
+# Use boxplot.stats() function to generate relevant outliers
+outlier_values <- boxplot.stats(states$Income)$out # outlier values.
+paste("Income outliers: ", paste(outlier_values, collapse=", "))
+
+#removing the outliers 
+# USA murder rates 3b pdf
+#"Population outliers:  21198, 11197, 18076, 11860, 12237"
+# "Income outliers:  6315"
+states <- subset(states, states$Population != 21198
+                 & states$Population != 11197 
+                 & states$Population != 18076
+                 & states$Population != 11860
+                 & states$Population != 12237)
+states <- subset(states, states$Income != 6315 )
+
+states
+
+#USA murder rates 3.c pdf
+
+
+install.packages("e1071")
+library(e1071)
+
+opar <- par(no.readonly = TRUE)
+#Show 4 rows x 2 cols of charts
+par(mfrow = c(4,2))
+par(opar)
+plot(density(states$Population),
+     main = "Density plot for population",
+     sub = paste("Skewness : ", round(e1071::skewness(states$Population), 2)))
+
+#to fill the area inside with color                 
+polygon(density(states$Population), col = "red")
+
+
+states
